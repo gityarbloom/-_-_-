680b8d7e92920c6b9c2878138cdcb0101066d10b
Israel-Yarbloom <i0504170922@gmail.com>
2026-04-27 01:38:54 +0300

    elastic-search loading is working

--- end_user_service/elastic_loader.py
+++ end_user_service/elastic_loader.py
@@ -1,7 +1,13 @@
 from moduls import ElasticSearchClient
+from moduls import MongoLoader
+import time
+import os
+
+
+
 def get_map():
     return {
-    "mappings": {
+        "mappings": {
         "properties": {
         "message_id": { "type": "keyword" },
         "suspect_id": { "type": "keyword" },
@@ -13,11 +19,11 @@ def get_map():
         "credit_rating_factor": { "type": "integer" },
         "final_score": { "type": "float" }
         }
-    }
-    }
+        }
+        }
 
 
-def transform(doc):
+def transform(doc:dict):
     return {
         "message_id": doc["message_id"],
         "suspect_id": doc["suspect_id"],
@@ -30,11 +36,25 @@ def transform(doc):
         "final_score": doc["final_score"]
     }
 
-els = ElasticSearchClient("http://localhost:9200", mapping=map)
+
+print("\n\n\n🌞 End-user service START!! 🌞\n\n\n")
+time.sleep(5)
+
+mongo = MongoLoader(os.getenv("MONGO_URI", "mongodb://admin:israelyarbloom@localhost:27017/?authSource=admin"))
+es = ElasticSearchClient("http://localhost:9200", "gate_keeper", mapping=get_map())
+counter = 0
+
+for doc in mongo.get_all_docs("gatekeeper_db", "gatekeeper_coll"):
+    counter += 1
+    print(f"\n\n****Received DOC number {counter}****")
+    print(f"DOC: \n{doc}\n")
+    es_doc = transform(doc)
+
+    response = es.index_doc(es_doc["message_id"], es_doc)
+    print("\nreceive document in ES index 'gate_keeper'")
+    print(f"doc: \n{response["result"]}\n"+"*"*75+"\n")
+    time.sleep(1)
 
 
-# (
-#     index="gatekeeper",
-#         id=doc["message_id"],
-#         document=transform(doc)
-#     )
+time.sleep(5)
+print("\n\n\n🥱 End-user service FINSHED!! 🥱\n\n\n")
--- end_user_service/moduls.py
+++ end_user_service/moduls.py
@@ -5,59 +5,60 @@ import time
 
 
 
-# class MongoLoader:
-#     def __init__(self, mongo_uri:str):
-#         self.client_conn = self.get_mongodb_client(mongo_uri)
+class MongoLoader:
+    def __init__(self, mongo_uri:str):
+        self.client_conn = self.get_mongodb_client(mongo_uri)
 
 
-#     def get_mongodb_client(self, mongo_uri):
-#         for retry in range(5):
-#             try:
-#                 time.sleep(2)
-#                 print("\nTry to connect to MongoDB⏳...")
-#                 client_conn = MongoClient(mongo_uri, serverSelectionTimeoutMS=2000)
-#                 client_conn.admin.command("ping")
-#                 print(f"\n👍 Cnnected!")
-#                 return client_conn
-#             except Exception as e:
-#                 print(f"\n👎 Attempt {retry+1} failed: {e}")
-#                 if retry == 4:
-#                     raise
+    def get_mongodb_client(self, mongo_uri):
+        for retry in range(5):
+            try:
+                time.sleep(2)
+                print("\nTry to connect to MongoDB⏳...")
+                client_conn = MongoClient(mongo_uri, serverSelectionTimeoutMS=2000)
+                client_conn.admin.command("ping")
+                print(f"\n👍 Cnnected!")
+                return client_conn
+            except Exception as e:
+                print(f"\n👎 Attempt {retry+1} failed: {e}")
+                if retry == 4:
+                    raise
 
     
-#     def insert(self, db:str, coll:str, docs:list[dict]):
-#         loader = self.client_conn[db][coll]
-#         if len(docs) > 1:
-#             print(f"\n\nInsert A batch of documents to MongoDB")
-#             try:
-#                 result = loader.insert_many(documents=docs)
-#                 print(f"\nInserted {len(result.inserted_ids)} docs\n")
-#             except BulkWriteError as e:
-#                 print("\nBulk write error:\n")
-#                 for err in e.details.get("\nwriteErrors", []):
-#                     print(f"\nFailed at index {err['index']}: {err['errmsg']}\n")
+    def insert(self, db:str, coll:str, docs:list[dict]):
+        loader = self.client_conn[db][coll]
+        if len(docs) > 1:
+            print(f"\n\nInsert A batch of documents to MongoDB")
+            try:
+                result = loader.insert_many(documents=docs)
+                print(f"\nInserted {len(result.inserted_ids)} docs\n")
+            except BulkWriteError as e:
+                print("\nBulk write error:\n")
+                for err in e.details.get("\nwriteErrors", []):
+                    print(f"\nFailed at index {err['index']}: {err['errmsg']}\n")
 
-#         print(f"\n\nInsert one doc to MongoDB")
-#         try:
-#             result = loader.insert_one(docs[0])
-#             print("\nInserted id:", result.inserted_id)
-#         except PyMongoError as e:
-#             print("\nMongo error:", e)
+        print(f"\n\nInsert one doc to MongoDB")
+        try:
+            result = loader.insert_one(docs[0])
+            print("\nInserted id:", result.inserted_id)
+        except PyMongoError as e:
+            print("\nMongo error:", e)
 
 
-#     def get_all_docs(self, db:str, coll:str):
-#         for doc in self.client_conn[db][coll].find():
-#             yield doc
+    def get_all_docs(self, db:str, coll:str):
+        for doc in self.client_conn[db][coll].find():
+            yield doc
 
 
 
 #########################################
-"****************************************"
-#########################################
+
+
 
 class ElasticSearchClient:
-    def __init__(self, host:str, mapping:dict):
+    def __init__(self, host:str, index_name:str, mapping:dict):
         self.es_host = host
+        self.index_name = index_name
         self.mapping = mapping
         self.es = self.start_es_conn()
 
@@ -68,21 +69,16 @@ class ElasticSearchClient:
                 time.sleep(2)
                 print("\nTry to connect to ElasticSearch⏳...")
                 es = Elasticsearch(self.es_host)
-                if es.ping():
-                    print(f"\n👍 ElasticSearch is cnnected!")
-                    return es
+                response = es.indices.create(index=self.index_name, body=self.mapping)
+                print(f"\n👍 ElasticSearch is cnnected!")
+                print(f"\n\n👍 Index {self.index_name} Created!\n{response}\n")
+                return es
             except Exception as e:
-                print(f"\n👎 Attempt {retry+1} failed: {e}")
+                print(f"\n👎 Attempt {retry+1} failed: {e}\n")
                 if retry == 4:
                     raise
 
 
-    def create_index(self, index_name:str):
-        response = self.es.indices.create(index=index_name, body=self.mapping)
-        print(f"\n\n👍 Index {index_name} Created!\n{response}\n")
-        self.index_name = index_name
-                
-                
     def index_doc(self, doc_id:str, document:dict):
         return self.es.index(
             index=self.index_name,
